3254f094d250a7b12d8733e6ea4c4abf31a3acca
Marcel Ribeiro <marcel.ribeiro@amadeus.com>
2015-07-26 17:16:56 +0200

    Adding configuration to allow continuous integration fia Shippable

--- clients/clients-impl/src/test-integration/java/com/namesfound/clients/merriamwebster/MerriamWebsterThesaurusImplILTest.java
+++ clients/clients-impl/src/test-integration/java/com/namesfound/clients/merriamwebster/MerriamWebsterThesaurusImplILTest.java
@@ -4,12 +4,7 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import com.namesfound.clients.helpers.config.ClientsHelpersConfig;
 import com.namesfound.clients.merriamwebster.config.MerriamWebsterConfig;
-import java.net.URI;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import junit.framework.TestCase;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,24 +16,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { MerriamWebsterConfig.class, ClientsHelpersConfig.class })
-public class MerriamWebsterThesaurusImplTest extends TestCase {
+public class MerriamWebsterThesaurusImplILTest extends TestCase {
 
   private static final String VALID_WORD = "merriamwebster";
   @Autowired
   private MerriamWebsterThesaurusImpl merriamWebsterThesaurusImpl;
 
-  @Ignore("The unmarshalling of XML is not working for this provider")
   @Test
-  public void testGetWebTarget() throws Exception {
-    //Given
-    Client client = ClientBuilder.newClient();
-    URI expectedTargetUri = new URI("http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/"+VALID_WORD+"?key=b5d05597-a001-4ffb-92cd-84225c0e2c56");
-
+  public void testGetTheSaurus() throws Exception {
     //when
-    WebTarget webTarget = merriamWebsterThesaurusImpl.getWebTarget(client, VALID_WORD);
+    Object responseValid = merriamWebsterThesaurusImpl.getTheSaurus("merriamwebster");
 
     //then
-    assertThat(webTarget).isNotNull();
-    assertThat(webTarget.getUri()).isEqualTo(expectedTargetUri);
+    assertThat(responseValid).isNotNull();
   }
 }
